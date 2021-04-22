68fea69d4bdc79fcca8b65b83739bbc7bb9e2e30
Azim Mustufa Baldiwala <83002384+azimbaldiwala@users.noreply.github.com>
2021-04-22 16:24:18 +0530

    Update HostFlask.py

--- HostFlask.py
+++ HostFlask.py
@@ -3,6 +3,6 @@ import os
 
 input("Before proceeding make sure your Flask app is running on 'localhost:500' : ")
 print("Press enter in all options for default settings")
-#os.system("ssh-keygen")
+os.system("ssh-keygen")       # Comment this out if you are not running this code for the first time or you have the ssh key already generated.
 os.system("ssh -R 80:localhost:5000 localhost.run")
 print("Your Flask Application is visible over internet on the link given above.")
